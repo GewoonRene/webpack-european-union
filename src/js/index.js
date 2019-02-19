c48d65337d9df1527144128c0b35b3deb6e1b28e
GewoonRene <rhuibertsjr@gmail.com>
2019-02-19 21:48:31 +0100

    Update: Drag movement

--- src/js/index.js
+++ src/js/index.js
@@ -1,28 +1,26 @@
 import '../scss/main.scss';
 
 // Variables
+let movement = document.getElementById('center')
 let imgComponent = document.getElementById('goodEU');
 let imgContainer = document.getElementById('left');
 
-// init
-init();
-function init() {
-    
-}
 
 // Event Handlers
 window.onresize = () => {
+    // Changes the width to the center of the device width
     imgComponent.style.width = window.innerWidth + 'px';
     imgContainer.style.width = window.innerWidth / 2 + 'px';
 };
 
-window.ondrag = (event) => {
+movement.ondrag = event => {
     // Changes the width value on drag event
     imgContainer.style.width = event.clientX + document.body.scrollLeft + 'px';
+    movement.style.left = event.clientX + document.body.scrollLeft + 'px';
 }
 
-window.ondragend = (event) => {
+movement.ondragend = event => {
     // Maintain changes of the width when stopped dragging
     imgContainer.style.width = event.clientX + document.body.scrollLeft + 'px';
+    movement.style.left = event.clientX + document.body.scrollLeft + 'px';
 }
-
