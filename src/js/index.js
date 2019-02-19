b433e50b305188fd372fb4fd215d9e6e261b96a3
GewoonRene <rhuibertsjr@gmail.com>
2019-02-19 11:15:10 +0100

    Fixed: Resize

--- src/js/index.js
+++ src/js/index.js
@@ -1,1 +1,20 @@
+// Imports
 import '../scss/main.scss';
+
+// Variables
+let goodImg, goodImgContainer, badImg, badImgContainer;
+
+// init
+init();
+function init() {
+    
+}
+
+// Event Handlers
+window.onresize = () => {
+    goodImg = document.getElementById('goodEU');
+    goodImgContainer = document.getElementById('left');
+
+    goodImg.style.width = window.innerWidth + 'px';
+    goodImgContainer.style.width = window.innerWidth / 2 + 'px';
+};
