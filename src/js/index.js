cc197f3c2c9ebd1ae828a83319d878339ddbe3a2
GewoonRene <rhuibertsjr@gmail.com>
2019-02-19 11:32:33 +0100

    Added: ondrag Event

--- src/js/index.js
+++ src/js/index.js
@@ -1,8 +1,8 @@
-// Imports
 import '../scss/main.scss';
 
 // Variables
-let goodImg, goodImgContainer, badImg, badImgContainer;
+let imgComponent = document.getElementById('goodEU');
+let imgContainer = document.getElementById('left');
 
 // init
 init();
@@ -12,9 +12,17 @@ function init() {
 
 // Event Handlers
 window.onresize = () => {
-    goodImg = document.getElementById('goodEU');
-    goodImgContainer = document.getElementById('left');
-
-    goodImg.style.width = window.innerWidth + 'px';
-    goodImgContainer.style.width = window.innerWidth / 2 + 'px';
+    imgComponent.style.width = window.innerWidth + 'px';
+    imgContainer.style.width = window.innerWidth / 2 + 'px';
 };
+
+window.ondrag = (event) => {
+    // Changes the width value on drag event
+    imgContainer.style.width = event.clientX + document.body.scrollLeft + 'px';
+}
+
+window.ondragend = (event) => {
+    // Maintain changes of the width when stopped dragging
+    imgContainer.style.width = event.clientX + document.body.scrollLeft + 'px';
+}
+
