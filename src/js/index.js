b1197adf355d04da8859435a90b790219f123272
GewoonRene <rhuibertsjr@gmail.com>
2019-03-06 16:36:26 +0100

    Added: Custom cursor

--- src/js/index.js
+++ src/js/index.js
@@ -1,28 +1,14 @@
 import '../scss/main.scss';
+import './cursor';
+// Importss
 
 // Variables
-let movement = document.getElementById('center')
-let imgComponent = document.getElementById('goodEU');
-let imgContainer = document.getElementById('left');
+const panelEl = document.getElementById('left');
 
 // Event Handlers
-// Resize window with device Width
-window.onresize = () => {
-    // Changes the width to the center of the device width
-    imgComponent.style.width = window.innerWidth + 'px';
-    imgContainer.style.width = window.innerWidth / 2 + 'px';
-};
+window.addEventListener('mousemove', event => {
+    // Panel follows mouse
+    panelEl.style.width = event.clientX + document.body.scrollLeft + 'px';
 
-// Changes width on drag
-window.ondrag = event => {
-    // Changes the width value on drag event
-    imgContainer.style.width = event.clientX + document.body.scrollLeft + 'px';
-    movement.style.left = event.clientX + document.body.scrollLeft + 'px';
-}
+});
 
-// Holds the current past position
-window.ondragend = event => {
-    // Maintain changes of the width when stopped dragging
-    imgContainer.style.width = event.clientX + document.body.scrollLeft + 'px';
-    movement.style.left = event.clientX + document.body.scrollLeft + 'px';
-}
